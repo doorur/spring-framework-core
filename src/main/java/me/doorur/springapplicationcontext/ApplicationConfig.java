28f0de17c1e98ec1ab948aa372d32444c21b6135
doorur <doorur@gmail.com>
2019-04-07 11:55:55 +0900

    [IOC컨테이너] 5. @SpringBooktApplication으로 컴포넌트 스캔

--- src/main/java/me/doorur/springapplicationcontext/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package me.doorur.springapplicationcontext;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackageClasses = DemoApplication.class)
-public class ApplicationConfig {
-}
--- src/main/java/me/doorur/springapplicationcontext/DemoApplication.java
+++ src/main/java/me/doorur/springapplicationcontext/DemoApplication.java
@@ -1,17 +1,11 @@
 package me.doorur.springapplicationcontext;
 
-import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+@SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-        System.out.println(Arrays.toString(beanDefinitionNames));
+        SpringApplication.run(DemoApplication.class, args);
     }
-
 }
