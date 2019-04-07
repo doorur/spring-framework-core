6c094797eca96848434b171c05af738582cc1a2f
doorur <doorur@gmail.com>
2019-04-07 22:35:25 +0900

    Singleton 스코프 빈과 Prototype 스코프 빈의 참조 : Proxy
      - Proxy를 통해 Singleton 빈이 Prototype 빈을 참조해도 매번 새로 생성하게 함

--- src/main/java/me/doorur/demo/Proto.java
+++ src/main/java/me/doorur/demo/Proto.java
@@ -1,9 +1,10 @@
 package me.doorur.demo;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Proto {
 }
